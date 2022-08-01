da083c037a209a7f0d20d5b22b4b5eee8b4cb428
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-08-02 02:57:02 +0300

    Import TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,39 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import {
+  AppConfigModule,
+  PostgresConfigModule,
+  PostgresConfigService,
+} from '@app/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { AppConfigModule } from '@app/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AppConfigModule, AuthModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [PostgresConfigModule],
+      useFactory: async (config: PostgresConfigService) => {
+        console.log(config.password);
+        return {
+          type: config.type,
+          host: config.host,
+          port: config.port,
+          username: config.username,
+          password: config.password,
+          database: config.database,
+        };
+      },
+      inject: [PostgresConfigService],
+    }),
+    AppConfigModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
