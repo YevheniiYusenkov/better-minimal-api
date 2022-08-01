68865564f60d79bec79344b3612ce6c835a4a46f
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-08-02 01:51:07 +0300

    Add users test requests

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { GetUsersDto } from './dto/get-users.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly service: UsersService) {}
+
+  @Post()
+  public async create(@Body() dto: CreateUserDto) {
+    return await this.service.create(dto);
+  }
+
+  @Get()
+  public async users(@Query() params: GetUsersDto) {
+    return await this.service.users(params);
+  }
+
+  @Get(':id')
+  public async user(@Param('id') id: string) {
+    return await this.service.user(id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
+import { CreateUserDto } from './dto/create-user.dto';
+import { GetUsersDto } from './dto/get-users.dto';
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  public async create(payload: CreateUserDto) {
+    return payload;
+  }
+
+  public async users(payload: GetUsersDto) {
+    return [{ id: 1, username: 'placeholder' }];
+  }
+
+  public async user(id: string) {
+    return { id, username: 'placeholder' };
+  }
+}
