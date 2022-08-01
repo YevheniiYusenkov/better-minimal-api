14e458306904bfe2c8dad83ec7f2fc438357a179
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-08-02 00:55:17 +0300

    Create AppConfigModule

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+
+import { AppConfigService } from '@app/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = app.get<AppConfigService>(AppConfigService);
+
+  await app.listen(config.port);
+
+  Logger.log(`App start listening at port: ${config.port}`);
 }
+
 bootstrap();
