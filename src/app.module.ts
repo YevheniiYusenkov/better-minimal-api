e5d8d111ddf621e99f67e7d6f71f32c9855e66ae
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-08-02 00:56:31 +0300

    Create AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { AppConfigModule } from '@app/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [AppConfigModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
