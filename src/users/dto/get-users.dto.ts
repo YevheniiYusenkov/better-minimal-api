727ef57f9ed16ad0d6367475cdc98cb191baabe4
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-08-02 01:50:35 +0300

    Create CreateUserDto, GetUsersDto

---
+++ src/users/dto/get-users.dto.ts
@@ -0,0 +1,9 @@
+import { IsNotEmpty, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class GetUsersDto {
+  @IsNotEmpty()
+  @Type(() => Number)
+  @IsNumber()
+  limit: number;
+}
