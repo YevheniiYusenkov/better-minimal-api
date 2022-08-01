4a1185e276872f5f53899b1d6b9ddf882660f4bf
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-08-02 01:35:05 +0300

    Create UserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 
 import { AppConfigModule } from '@app/config';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AppConfigModule, AuthModule],
+  imports: [AppConfigModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
