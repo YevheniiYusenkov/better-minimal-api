2bbc75e21ccd4677ab3c42a093d1387eb4d5f237
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-08-02 06:16:51 +0300

    Using Crypto in AuthModule

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
+
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly service: AuthService) {}
 
   @Post('login')
-  public async login(@Body() dto: { id: string }) {
-    return await this.service.login(dto.id);
+  public async login(@Body() dto: LoginDto) {
+    return await this.service.login(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@nestjs/common';
+
 import { UsersService } from '../users/users.service';
+import { CryptoService } from '../crypto/crypto.service';
+
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly crypto: CryptoService,
+  ) {}
 
-  public async login(id: string) {
-    return await this.usersService.user({
-      id: id,
+  public async login(payload: LoginDto) {
+    const user = await this.usersService.user({
+      username: payload.username,
       addSelect: 'password',
     });
+
+    return await this.crypto.compare(payload.password, user.password);
   }
 }
