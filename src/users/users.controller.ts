ec2e1a6554c0648e5a21b8d16c4263faf1645b13
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-08-02 13:32:03 +0300

    Using JwtAuthGuard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+
+import { JwtAuthGuard } from '@app/guards';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { GetUsersDto } from './dto/get-users.dto';
@@ -14,11 +24,13 @@ export class UsersController {
     return await this.service.create(dto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   public async users(@Query() query: GetUsersDto) {
     return await this.service.users(query);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   public async user(@Param() params: GetUserDto) {
     return await this.service.user(params);
