8d03397292dc82b8e75c0f49514b13875680adc0
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-08-02 05:24:38 +0300

    Add test login request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly service: AuthService) {}
+
+  @Post('login')
+  public async login(@Body() dto: { id: string }) {
+    return await this.service.login(dto.id);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  public async login(id: string) {
+    return await this.usersService.user({
+      id: id,
+      addSelect: 'password',
+    });
+  }
+}
