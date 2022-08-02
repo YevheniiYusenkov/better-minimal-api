b813dd6b2b75915ed7c2a416f4515993509508cb
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-08-02 06:16:32 +0300

    Update crypto.service.ts

--- src/crypto/crypto.service.ts
+++ src/crypto/crypto.service.ts
@@ -1,6 +1,18 @@
+import * as bcrypt from 'bcrypt';
+
 import { Injectable } from '@nestjs/common';
+
+import { BcryptConfigService } from '@app/config';
 
 @Injectable()
 export class CryptoService {
-  constructor() {}
+  constructor(private readonly config: BcryptConfigService) {}
+
+  public async hash(value: string): Promise<string> {
+    return await bcrypt.hash(value, this.config.salt);
+  }
+
+  public async compare(value: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(value, hash);
+  }
 }
