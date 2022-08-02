842b11795488307ef7e9c47015d2066ce3454ff1
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-08-02 05:34:36 +0300

    Export BcryptConfigModule

--- libs/config/src/index.ts
+++ libs/config/src/index.ts
@@ -3,3 +3,6 @@ export * from './app/config.service';
 
 export * from './postgres/config.module';
 export * from './postgres/config.service';
+
+export * from './bcrypt/config.module';
+export * from './bcrypt/config.service';
